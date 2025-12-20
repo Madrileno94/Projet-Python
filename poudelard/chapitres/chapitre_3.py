4930759770d89e7cc64da688fbef23c6bd9af6ea
Madrileno94 <kajeban.mahendran@efrei.net>
2025-12-20 23:18:21 +0100

    chap3

---
+++ poudelard/chapitres/chapitre_3.py
@@ -0,0 +1,70 @@
+import random
+from utils.input_utils import load_fichier
+
+
+def apprendre_sorts(joueur, chemin_fichier="data/sorts.json"):
+    print("Tu commences tes cours de magie à Poudlard...\n")
+
+    tous_les_sorts = load_fichier(chemin_fichier)
+
+    off = [s for s in tous_les_sorts if s["type"].lower() == "offensif"]
+    defen = [s for s in tous_les_sorts if s["type"].lower() == "défensif"]
+    util = [s for s in tous_les_sorts if s["type"].lower() == "utilitaire"]
+
+    choisis = []
+    choisis.extend(random.sample(off, 1))
+    choisis.extend(random.sample(defen, 1))
+    choisis.extend(random.sample(util, 3))
+
+    if "Sortilèges" not in joueur:
+        joueur["Sortilèges"] = []
+
+    for s in choisis:
+        joueur["Sortilèges"].append(s["nom"])
+        print(f"Tu viens d'apprendre le sortilège : {s['nom']} ({s['type']})")
+        input("Appuie sur Entrée pour continuer...")
+
+    print("\nTu as terminé ton apprentissage de base à Poudlard !")
+    print("Voici les sortilèges que tu maîtrises désormais :\n")
+
+    for s in choisis:
+        print(f"- {s['nom']} ({s['type']}) : {s['description']}")
+
+def quiz_magie(joueur, chemin_fichier="data/quiz_magie.json"):
+    print("Bienvenue au quiz de magie de Poudlard !")
+    print("Réponds correctement aux 4 questions pour faire gagner des points à ta maison.\n")
+
+    questions_data = load_fichier(chemin_fichier)
+    questions_choisies = []
+
+    while len(questions_choisies) < 4:
+        q = random.choice(questions_data)
+        if q not in questions_choisies:
+            questions_choisies.append(q)
+
+    score_quiz = 0
+
+    for i in range(len(questions_choisies)):
+        q = questions_choisies[i]
+        print(f"{i + 1}. {q['question']}")
+
+        reponse_joueur = input("> ")
+
+        if reponse_joueur.lower() == q['reponse'].lower():
+            print("Bonne réponse ! +25 points pour ta maison.")
+            score_quiz += 25
+        else:
+            print(f"Mauvaise réponse. La bonne réponse était : {q['reponse']}")
+
+    print(f"Score obtenu : {score_quiz} points")
+
+    if "Score" not in joueur:
+        joueur["Score"] = 0
+    joueur["Score"] += score_quiz
+
+
+
+
+
+
+
